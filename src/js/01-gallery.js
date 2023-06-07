8b29f17cd80190b983a9488b5650a59d0019d01d
Koval0050 <121100028+Koval0050@users.noreply.github.com>
2023-06-07 13:07:27 +0300

    fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const imgContainer = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const imgGallery = galleryItems.map(elem => {
+  return `<li class="gallery__item">
+   <a class="gallery__link" href="${elem.original}">
+      <img class="gallery__image" src="${elem.preview}" alt="${elem.description}" />
+   </a>
+</li>`;
+});
+
+imgContainer.insertAdjacentHTML('beforeend', imgGallery.join(''));
+
+function showModal(e) {
+  e.preventDefault();
+  //   const captionEl = e.target.alt;
+  //   я так і не зміг розібратись як зробити підпис
+
+  var $gallery = new SimpleLightbox('.gallery a', {});
+}
+
+imgContainer.addEventListener('click', showModal);
